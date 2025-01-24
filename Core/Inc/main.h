d9813188f38bf65a7fdd4d0fa581e6afa8a00654
Victor Karaboychev <43865100+VictorKaraboychev@users.noreply.github.com>
2025-01-23 20:54:38 -0500

    Framework
    Generated by STM32CubeMX

---
+++ Core/Inc/main.h
@@ -0,0 +1,103 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define BUZZER_PA2_Pin GPIO_PIN_2
+#define BUZZER_PA2_GPIO_Port GPIOA
+#define V_SENSE_1_Pin GPIO_PIN_13
+#define V_SENSE_1_GPIO_Port GPIOF
+#define V_SENSE_2_Pin GPIO_PIN_14
+#define V_SENSE_2_GPIO_Port GPIOF
+#define GPIO_PB12_Pin GPIO_PIN_12
+#define GPIO_PB12_GPIO_Port GPIOB
+#define GPIO_PB13_Pin GPIO_PIN_13
+#define GPIO_PB13_GPIO_Port GPIOB
+#define GPIO_PB14_Pin GPIO_PIN_14
+#define GPIO_PB14_GPIO_Port GPIOB
+#define GPIO_PB15_Pin GPIO_PIN_15
+#define GPIO_PB15_GPIO_Port GPIOB
+#define GPIO_PD8_Pin GPIO_PIN_8
+#define GPIO_PD8_GPIO_Port GPIOD
+#define GPIO_PD9_Pin GPIO_PIN_9
+#define GPIO_PD9_GPIO_Port GPIOD
+#define GPIO_PD10_Pin GPIO_PIN_10
+#define GPIO_PD10_GPIO_Port GPIOD
+#define GPIO_PD11_Pin GPIO_PIN_11
+#define GPIO_PD11_GPIO_Port GPIOD
+#define BAR_DDRY_Pin GPIO_PIN_0
+#define BAR_DDRY_GPIO_Port GPIOD
+#define BAR_CS_Pin GPIO_PIN_1
+#define BAR_CS_GPIO_Port GPIOD
+#define IMU_INT2_Pin GPIO_PIN_3
+#define IMU_INT2_GPIO_Port GPIOD
+#define IMU_INT1_Pin GPIO_PIN_4
+#define IMU_INT1_GPIO_Port GPIOD
+#define IMU_CS_Pin GPIO_PIN_5
+#define IMU_CS_GPIO_Port GPIOD
+#define MAG_CS_Pin GPIO_PIN_12
+#define MAG_CS_GPIO_Port GPIOG
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Core/Src/freertos.c
@@ -0,0 +1,216 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 512 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for imuTask */
+osThreadId_t imuTaskHandle;
+const osThreadAttr_t imuTask_attributes = {
+  .name = "imuTask",
+  .stack_size = 512 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for magTask */
+osThreadId_t magTaskHandle;
+const osThreadAttr_t magTask_attributes = {
+  .name = "magTask",
+  .stack_size = 512 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for barTask */
+osThreadId_t barTaskHandle;
+const osThreadAttr_t barTask_attributes = {
+  .name = "barTask",
+  .stack_size = 512 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for gpsTask */
+osThreadId_t gpsTaskHandle;
+const osThreadAttr_t gpsTask_attributes = {
+  .name = "gpsTask",
+  .stack_size = 512 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for spi1Mutex */
+osMutexId_t spi1MutexHandle;
+const osMutexAttr_t spi1Mutex_attributes = {
+  .name = "spi1Mutex"
+};
+/* Definitions for spi4Mutex */
+osMutexId_t spi4MutexHandle;
+const osMutexAttr_t spi4Mutex_attributes = {
+  .name = "spi4Mutex"
+};
+/* Definitions for i2c1Mutex */
+osMutexId_t i2c1MutexHandle;
+const osMutexAttr_t i2c1Mutex_attributes = {
+  .name = "i2c1Mutex"
+};
+/* Definitions for usbMutex */
+osMutexId_t usbMutexHandle;
+const osMutexAttr_t usbMutex_attributes = {
+  .name = "usbMutex"
+};
+/* Definitions for fdcanMutex */
+osMutexId_t fdcanMutexHandle;
+const osMutexAttr_t fdcanMutex_attributes = {
+  .name = "fdcanMutex"
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+extern void StartImuTask(void *argument);
+extern void StartMagTask(void *argument);
+extern void StartBarTask(void *argument);
+extern void StartGpsTask(void *argument);
+
+extern void MX_USB_DEVICE_Init(void);
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+  /* Create the mutex(es) */
+  /* creation of spi1Mutex */
+  spi1MutexHandle = osMutexNew(&spi1Mutex_attributes);
+
+  /* creation of spi4Mutex */
+  spi4MutexHandle = osMutexNew(&spi4Mutex_attributes);
+
+  /* creation of i2c1Mutex */
+  i2c1MutexHandle = osMutexNew(&i2c1Mutex_attributes);
+
+  /* creation of usbMutex */
+  usbMutexHandle = osMutexNew(&usbMutex_attributes);
+
+  /* creation of fdcanMutex */
+  fdcanMutexHandle = osMutexNew(&fdcanMutex_attributes);
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of imuTask */
+  imuTaskHandle = osThreadNew(StartImuTask, NULL, &imuTask_attributes);
+
+  /* creation of magTask */
+  magTaskHandle = osThreadNew(StartMagTask, NULL, &magTask_attributes);
+
+  /* creation of barTask */
+  barTaskHandle = osThreadNew(StartBarTask, NULL, &barTask_attributes);
+
+  /* creation of gpsTask */
+  gpsTaskHandle = osThreadNew(StartGpsTask, NULL, &gpsTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* init code for USB_DEVICE */
+  MX_USB_DEVICE_Init();
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
