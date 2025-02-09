153c0930f0672d59180accf1ada81668f6ed121d
Jacob Gordon <jacobgordon1003@gmail.com>
2025-02-09 21:59:38 +0000

    Implemented Pure Pursit V1

---
+++ Core/Src/utility/pure_pursuit.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include "control.h"
+#include "sensors.h"
+#include "vector.h"
+
+float calculateCurvatureRadius(const State& robot, const Vector& target) {
+    // Calculate the relative position of the target point
+    float dx = *target.x - *robot.position.x;
+    float dy = *target.y - *robot.position.y;
+
+    // Find angle between heading and target
+    float target_angle = atan2(dy, dx);
+    float angle_diff = target_angle - robot.orientation;
+
+    //Calculate Curvatire
+    return 2*sin(angle_diff) / hypot(dx,dy);
+}
+
+float calculateSpeedRatio(float cuvature_radius){
+    return cuvature_radius / WHEEL_DISTANCE;
+}
