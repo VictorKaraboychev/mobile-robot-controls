16216d7f3acba151e5775e8763e49e1849a1318a
Victor Karaboychev <43865100+VictorKaraboychev@users.noreply.github.com>
2025-02-13 17:28:58 -0500

    Fixed wheel radius define

--- Core/Inc/sensors.h
+++ Core/Inc/sensors.h
@@ -20,7 +20,7 @@
 #define RAD_TO_DEG 57.295779513082320876798154814105f
 
 #define PULSE_PER_REVOLUTION 4096.0f									// pulses per revolution
-#define WHEEL_RADIUS 0.074f												// m
+#define WHEEL_RADIUS 0.037f												// m
 #define WHEEL_CIRCUMFERENCE (WHEEL_RADIUS * M_TWOPI)					// m
 #define DISTANCE_PER_PULSE (WHEEL_CIRCUMFERENCE / PULSE_PER_REVOLUTION) // m
 #define WHEEL_DISTANCE 0.180f											// m
